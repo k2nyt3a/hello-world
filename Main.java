d4abe5a3dc8d94c75f83302e99b230db0ea16c99
k2nyt3a <kanyanatwasutawat@gmail.com>
2025-04-11 13:09:50 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
-public class wHatIsThIssEvenDoing{
-  
-  public static void main(String[] args){
-    int[] numbrz = {5, 2, 8, 1, 3};
-    sortz(numbrz);
-    for(int i=0;i<numbrz.length;++i)
-    System.out.println(numbrz[i])
+public class SortExample {
+
+  public static void main(String[] args) {
+    int[] numbers = {5, 2, 8, 1, 3};
+    bubbleSort(numbers);
+    for (int i = 0; i < numbers.length; ++i) {
+      System.out.println(numbers[i]);
+    }
   }
 
-  
-  public static void sortz(int[] wat){ 
-    for(int i=0;i<wat.length;i++){
-      for(int j=0;j<wat.length;j++){
-        if(wat[i]<wat[j]){
-          int tmp = wat[i];
-          wat[i]=wat[j];
-          wat[j]= tmp;
+  public static void bubbleSort(int[] array) {
+    for (int i = 0; i < array.length - 1; i++) {
+      for (int j = 0; j < array.length - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
         }
       }
     }
   }
 }
+
